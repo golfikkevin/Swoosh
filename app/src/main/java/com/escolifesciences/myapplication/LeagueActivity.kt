634fe0bcd9aefb4d00886a03ab54b263780827bc
kevin.golfik <kevin.golfik@escolifesciences.com>
2024-06-20 13:44:42 +0700

    commit lesson 4 swoosh

--- app/src/main/java/com/escolifesciences/myapplication/LeagueActivity.kt
+++ app/src/main/java/com/escolifesciences/myapplication/LeagueActivity.kt
@@ -1,12 +1,14 @@
 package com.escolifesciences.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +18,10 @@ class LeagueActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun leagueNextClicked (view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
--- app/src/main/java/com/escolifesciences/myapplication/SkillActivity.kt
+++ app/src/main/java/com/escolifesciences/myapplication/SkillActivity.kt
@@ -1,28 +1,20 @@
 package com.escolifesciences.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : BaseActivity() {
+class SkillActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_skill)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val btnGetStarted = findViewById<Button>(R.id.btnGetStarted)
-
-        btnGetStarted.setOnClickListener {
-            val leagueIntent = Intent(this, LeagueActivity::class.java)
-            startActivity(leagueIntent)
         }
     }
 }
