5590593107e47bd043a83aa85b7618b3fecf0f47
kevin.golfik <kevin.golfik@escolifesciences.com>
2024-06-20 11:26:02 +0700

    commit lesson 3 swoosh

--- app/src/main/java/com/escolifesciences/myapplication/MainActivity.kt
+++ app/src/main/java/com/escolifesciences/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.escolifesciences.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnGetStarted = findViewById<Button>(R.id.btnGetStarted)
+
+        btnGetStarted.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
         }
     }
 }
